3afdeb588569679658b00ff1593e2be59d67b830
Phuc Huynh <phuc.huynh@kyanon.digital>
2016-08-05 18:51:48 +0700

    demo game

---
+++ flipcard-demo/www/js/app.js
@@ -0,0 +1,54 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var gameCard =angular.module('starter', ['ionic','LocalStorageModule']);
+
+gameCard.run(function($ionicPlatform,CacheService) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs).
+      // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
+      // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
+      // useful especially with forms, though we would prefer giving the user a little more room
+      // to interact with the app.
+      // if (window.cordova && window.cordova.plugins.Keyboard) {
+      //   cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      //   cordova.plugins.Keyboard.disableScroll(true);
+      // }
+      if (window.StatusBar) {
+        // Set the statusbar to use the default style, tweak this to
+        // remove the status bar on iOS or change it to use white instead of dark colors.
+        StatusBar.styleDefault();
+      }
+    });
+  ionic.Platform.fullScreen();
+  if(CacheService.get('numCards')===null){
+    CacheService.set('numCards',10);
+  };
+
+  if(CacheService.get('gameCard')===null){
+     CacheService.set('gameCard','m3')
+  };
+  });
+
+
+gameCard.config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home',{
+      url:"/home",
+      templateUrl:'page/home.html'
+    })
+
+    .state('demo',{
+      url:'/demo',
+      templateUrl:'page/game.html'
+    })
+
+  $urlRouterProvider.otherwise('/home');
+});
+
+
+
+
---
+++ flipcard-demo/www/js/controller.js
@@ -0,0 +1,41 @@
+/**
+ * Created by phuch on 8/5/2016.
+ */
+
+gameCard.controller('GameCtrl', function GameCtrl($scope, $state, game, CacheService) {
+
+  $scope.cardSize = (window.innerWidth) / 5.5;
+  $scope.numPairs = CacheService.get('numCards');
+  $scope.gameCards = CacheService.get('gameCards');
+  $scope.game = game.newGame();
+
+  $scope.setNumberPairs = function(numCards){
+    CacheService.set('numCards', numCards);
+    //console.log('setting number of cards: ' + numCards);
+    $scope.game = game.newGame();
+    $state.go('initial');
+  }
+
+  $scope.setGameCards = function(gameCards){
+    CacheService.set('gameCards', gameCards);
+    //console.log(gameCards);
+    $scope.game = game.newGame();
+    $state.go('initial');
+  }
+});
+
+gameCard.controller('MainCtrl', function MainCtrl($scope, $state, game, CacheService) {
+
+  $scope.numPairs = CacheService.get('numCards');
+  $scope.startGame = function(){
+    $state.go('gameBoard', {}, { reload: true });
+  }
+
+  $scope.gameOptions = function(){
+    $state.go('gameLevel');
+  }
+
+  $scope.gameCards = function(){
+    $state.go('gameCards');
+  }
+});
